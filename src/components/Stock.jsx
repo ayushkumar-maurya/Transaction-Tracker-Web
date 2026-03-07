0febfd9feec7492e79584231854f60a5290d391f
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-03-07 19:29:09 +0530

    Added Stock Component

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,10 +1,60 @@
-import { Link } from "react-router-dom"
+import { useState, useRef, useEffect } from 'react'
+import styles from '../styles/stockStyles'
+import MenuItem from "./Utility/MenuItem"
 
 const Stock = () => {
+  const iconDirPath = '/stock/'
+  const [nEmptyItemsToAdd, setNEmptyItemsToAdd] = useState(null)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const calcNEmptyItemsToAdd = () => {
+      let containerWidth = containerRef.current.offsetWidth
+      // 20 is the container padding.
+      // 250 + 20 is the item's width + margin.
+      let nItemsInRow = Math.trunc((containerWidth - 20) / (250 + 20))
+      let nItemsInLastRow = menuOptions.length % nItemsInRow
+      if(nItemsInLastRow === 0)
+        nItemsInLastRow = nItemsInRow
+      return nItemsInRow - nItemsInLastRow
+    }
+
+    setNEmptyItemsToAdd(calcNEmptyItemsToAdd())
+
+    const handleResize = () => {
+      setNEmptyItemsToAdd(calcNEmptyItemsToAdd())
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+
+  const menuOptions = [
+  { path: '/stock/add-transaction', iconPath: `${iconDirPath}add-transaction.png`, title: 'Add Transaction' }
+  ]
+
   return (
-    <>
-      <Link to='/stock/add-transaction'>Add Transaction</Link>
-    </>
+    <div className='container-fluid' style={styles.container} ref={containerRef}>
+      <div style={styles.childContainer}>
+
+        {menuOptions.map((opt, index) =>
+          <MenuItem
+            path={opt.path}
+            iconPath={opt.iconPath}
+            title={opt.title}
+            key={index}
+          />
+        )}
+
+        {Array.from({length: nEmptyItemsToAdd}).map((_, i) =>
+          <div style={styles.empty} key={i} />
+        )}
+
+      </div>
+    </div>
   )
 }
 
---
+++ src/styles/stockStyles.jsx
@@ -0,0 +1,27 @@
+import colours from "../colours";
+
+const styles = {
+  container: {
+    backgroundColor: colours.screenBg,
+    padding: 0
+  },
+
+  childContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+
+  empty: {
+    height: 150,
+    width: 250,
+    margin: 10
+  }
+}
+
+export default styles
