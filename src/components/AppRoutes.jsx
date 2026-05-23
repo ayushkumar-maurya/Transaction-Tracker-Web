5469e42282bb71125acce4d3f431859c52070d35
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-05-23 20:21:40 +0530

    Corrected routes for updating a transaction

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -21,7 +21,7 @@ const AppRoutes = () => {
           <Route index element={<Navigate to='/bank/transaction/add' />} />
           <Route path='add' element={<EditTransaction {...editTransaction.bank.add} />} />
           <Route path='transactions' element={<Transactions {...transactions.bank} />} />
-          <Route path='update' element={<EditTransaction {...editTransaction.bank.add} />} />
+          <Route path='update' element={<EditTransaction {...editTransaction.bank.update} />} />
         </Route>
 
         <Route path='banks' element={<Categories {...categories.bank} />} />
@@ -36,7 +36,7 @@ const AppRoutes = () => {
           <Route index element={<Navigate to='/creditcard/transaction/add' />} />
           <Route path='add' element={<EditTransaction {...editTransaction.creditCard.add} />} />
           <Route path='transactions' element={<Transactions {...transactions.creditCard} />} />
-          <Route path='update' element={<EditTransaction {...editTransaction.creditCard.add} />} />
+          <Route path='update' element={<EditTransaction {...editTransaction.creditCard.update} />} />
         </Route>
 
         <Route path='creditcards' element={<Categories {...categories.creditCard} />} />
@@ -51,7 +51,7 @@ const AppRoutes = () => {
           <Route index element={<Navigate to='/stock/transaction/add' />} />
           <Route path='add' element={<EditTransaction {...editTransaction.stock.add} />} />
           <Route path='transactions' element={<Transactions {...transactions.stock} />} />
-          <Route path='update' element={<EditTransaction {...editTransaction.stock.add} />} />
+          <Route path='update' element={<EditTransaction {...editTransaction.stock.update} />} />
         </Route>
 
         <Route path='stocks' element={<Categories {...categories.stock} />} />
@@ -66,7 +66,7 @@ const AppRoutes = () => {
           <Route index element={<Navigate to='/mutualfund/transaction/add' />} />
           <Route path='add' element={<EditTransaction {...editTransaction.mutualFund.add} />} />
           <Route path='transactions' element={<Transactions {...transactions.mutualFund} />} />
-          <Route path='update' element={<EditTransaction {...editTransaction.mutualFund.add} />} />
+          <Route path='update' element={<EditTransaction {...editTransaction.mutualFund.update} />} />
         </Route>
 
         <Route path='mutualfunds' element={<Categories {...categories.mutualFund} />} />
@@ -81,7 +81,7 @@ const AppRoutes = () => {
           <Route index element={<Navigate to='/misc/transaction/add' />} />
           <Route path='add' element={<EditTransaction {...editTransaction.misc.add} />} />
           <Route path='transactions' element={<Transactions {...transactions.misc} />} />
-          <Route path='update' element={<EditTransaction {...editTransaction.misc.add} />} />
+          <Route path='update' element={<EditTransaction {...editTransaction.misc.update} />} />
         </Route>
 
         <Route path='misccategories' element={<Categories {...categories.misc} />} />
