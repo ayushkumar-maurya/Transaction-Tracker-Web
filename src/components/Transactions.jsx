1b9c6d04ead93e4bf60b67e592eb6d9d3c5e585c
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-05-23 12:01:20 +0530

    Fetching transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,6 +1,53 @@
-const Transactions = () => {
+import { useState, useEffect, useRef } from 'react'
+import { API_URL } from '../config'
+import styles from "../styles/components/TransactionsStyles"
+import Alert from './Alert'
+import Spinner from './Spinner'
+import '../css/components/Transactions.css'
+
+const Transactions = ({ title, path, updatePath }) => {
+  const [transactions, setTransactions] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const alertRef = useRef(null)
+  const [showAlert, setShowAlert] = useState(false)
+
+  const getTransactions = async () => {
+    try {
+      const url = `${API_URL}${path}`
+      let params = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      }
+      const res = await fetch(url, params)
+      setLoading(false)
+      if(!res)
+        throw new Error('Unable to fetch transactions!')
+      let resData = await res.json()
+      if(!(resData && Array.isArray(resData)))
+        throw new Error('Unable to fetch transactions!')
+      setTransactions(resData)
+    }
+    catch(err) {
+      alertRef.current = { title: title, msg: err.message }
+      setShowAlert(true)
+    }
+  }
+
+  useEffect(() => {
+    getTransactions()
+  }, [])
+
   return (
-    <p>Transactions!</p>
+    <>
+      { loading && <Spinner /> }
+      <div className="items-container" style={styles.itemsContainer}>
+        {transactions.map((item, index) =>
+          <p key={index}>{ item.category_name }</p>
+        )}
+      </div>
+      <Alert infoRef={alertRef} showFlag={showAlert} updateShowFlag={setShowAlert} />
+    </>
   )
 }
 
